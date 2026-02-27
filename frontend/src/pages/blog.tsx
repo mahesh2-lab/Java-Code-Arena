1419e514fa7b0ca52c9c751e0313415e2725bbf6
mahesh2-lab <maheshchopade133@gmail.com>
2026-02-27 23:40:21 +0530

    feat: remove BlogPage component as routing is now handled by backend

--- frontend/src/pages/blog.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown";
-import rehypeHighlight from "rehype-highlight";
-import rehypeRaw from "rehype-raw";
-import remarkGfm from "remark-gfm";
-
-// BlogPage component removed. Blog routing handled by backend.
-// BlogPage component removed. Blog routing handled by backend.
-export default null;
